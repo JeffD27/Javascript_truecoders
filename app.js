18c85fac2e48d6cad05005b211d3926cd756b013
Jeffrey D'Angelo <jeffrey.dangelo1@gmail.com>
2023-03-05 15:26:40 -0500

    completed javascript intro excercises

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName 
+let lastName = "D'Angelo"
+var age = 35
 // Exercise 2
+firstName = "Jeff"
+lastName = "D'Angelo"
+age = "35"
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old`)
 
 // Exercise 3
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+let price = 19.99
+let isOnSale = false
+let salePercentage = 13
+let stock = 0
+let inStock = false 
+let selectedSize = "M"
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
